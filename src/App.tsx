154266c305705def02fc3f50450b4b15ad75e9bd
alonshuld <alonshuld@gmail.com>
2025-10-03 18:40:41 +0300

    added logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { Logo } from "./components/common/Logo";
+
+import "./App.css";
+
+const theme = createTheme({
+  typography: {
+    h1: {
+      fontSize: "3rem",
+      fontWeight: 600,
+    },
+  },
+});
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <ThemeProvider theme={theme}>
+      <Logo />
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/common/Logo.tsx
@@ -0,0 +1,14 @@
+import { type FC } from "react";
+import { Box, Typography } from "@mui/material";
+import { Album } from "@mui/icons-material";
+
+export const Logo: FC = () => {
+  return (
+    <Box className="logo" display="flex" alignItems="center">
+      <Album sx={{ fontSize: "3rem", margin: 0 }} color="primary" />
+      <Typography variant="h1" color="primary">
+        din
+      </Typography>
+    </Box>
+  );
+};
