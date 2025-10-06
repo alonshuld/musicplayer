4b75535ceb007fa3c39a8c4fd4cce9376397ad40
alonshuld <alonshuld@gmail.com>
2025-10-06 23:29:21 +0300

    moved useSongs

--- src/api/useSongs.ts
+++ src/api/useSongs.ts
@@ -2,21 +2,12 @@ import { useQuery } from "@tanstack/react-query";
 
 const API_URL = "http://localhost:3001";
 
-export interface Song {
-  id: number,
-  name: string,
-  artist: string,
-  cover: string,
-  sound: string,
-}
-
 const fetcher = async (endpoint: string) => {
   const res = await fetch(`${API_URL}/${endpoint}`);
   if (!res.ok) throw new Error("Failed to fetch " + endpoint);
   return res.json();
 };
 
-// Correct TypeScript version
 export const useSongs = () => {
   return useQuery({
     queryKey: ["songs"],
