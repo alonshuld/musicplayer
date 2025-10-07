604b06f31f7d22790d50b156db8b4e6bd05d2d07
alonshuld <alonshuld@gmail.com>
2025-10-07 04:44:25 +0300

    updated the homepage

--- src/App.tsx
+++ src/App.tsx
@@ -9,11 +9,11 @@ import { QueueDrawer } from "./components/common/QueueDrawer";
 import { SongCover } from "./components/common/SongCover";
 import { Error } from "./components/common/Error";
 import { PlayBtn } from "./components/common/PlayBtn";
+import { Volume } from "./components/common/Volume";
 import { useSongs } from "./api/useSongs";
 import { type Song, usePlayer } from "./hooks/usePlayer";
 
 import "./App.css";
-import { PauseBtn } from "./components/common/PauseBtn";
 
 const theme = createTheme({
   components: {
@@ -157,11 +157,12 @@ function App() {
               />
             ) : null,
           ]}
-          center={[
-            <PauseBtn isPlaying={isPlaying} togglePlay={togglePlay} />,
-            <div>---------------</div>,
+          center={[<span>[ || ]</span>, <div>---------------</div>]}
+          right={[
+            <Volume volume={volume} setVolume={setVolume} />,
+            <div>shuffle</div>,
+            <div>repeat</div>,
           ]}
-          right={[<div>volume</div>, <div>shuffle</div>, <div>repeat</div>]}
         />
       </Box>
     </ThemeProvider>
